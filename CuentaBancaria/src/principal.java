4c20ab4cee5ffca56bea697f9596091f17d1267b
jacin-stack <128541823+jacin-stack@users.noreply.github.com>
2024-01-25 17:16:22 +0000

    Add files via upload

---
+++ CuentaBancaria/src/principal.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class principal {
+
+	public static void main(String[] args) {
+		String iban, nombre, pin;
+		double saldo;
+		Scanner sc = new Scanner (System.in);
+		
+		cuentas c1 = new cuentas();
+		c1.setTitular("Javier");
+		System.out.println("Iban: "+c1.getIBAN());
+		System.out.println("Titular: "+c1.getTitular());
+		
+		
+		System.out.println("Dime el iban: ");
+		iban=sc.next();
+		System.out.println("Dime el titular: ");
+		nombre=sc.next();
+		cuentas c2 = new cuentas(iban,nombre);
+		System.out.println("Iban2: "+c2.getIBAN());
+		System.out.println("Titular2: "+c2.getTitular());
+		System.out.println("Saldo2: "+c2.getSaldo());
+		
+		
+	}
+
+}
